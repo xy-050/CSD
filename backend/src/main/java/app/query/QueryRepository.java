8a457cff08a486116209514da02cd12d15c83dd9
Elodie <elodieyeung@gmail.com>
2025-09-18 11:31:46 +0800

    query merge

--- backend/src/main/java/app/query/QueryRepository.java
+++ backend/src/main/java/app/query/QueryRepository.java
@@ -1,11 +1,12 @@
 package app.query;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Repository
-public class QueryRepository extends JpaRepository<Query, Long> {
+public interface QueryRepository extends JpaRepository<Query, Long> {
     // Repository implementation goes here
 
 }
