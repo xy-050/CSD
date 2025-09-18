c9ab2c62fd1ce15859e2d9100845c82cfb6d6c5c
Elodie <elodieyeung@gmail.com>
2025-09-18 11:20:27 +0800

    FRONTEND

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import App from './App.js';
-import LoginPage from './components/LoginPage.js';
-import SignupPage from './components/SignupPage.js';
-import HomePage from './components/Homepage.js';
+// import App from './App.js';
+import LoginPage from './components/LoginPage.jsx';
+import SignupPage from './components/SignupPage.jsx';
+import HomePage from './components/Homepage.jsx';
 import './App.css';
 
 const App = () => {
--- view/SwaggerConfig.java
+++ view/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package app.view;
+package view;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
