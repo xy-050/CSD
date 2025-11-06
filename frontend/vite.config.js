243a7407f98cf2d29ab5ad9f30decce2f2c9894c
xy-050 <sapphire32181@gmail.com>
2025-11-06 15:10:02 +0800

    changed stuff for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import reactSWC from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactSWC()],
   server: {
     host: true, 
     port: 5173, 
